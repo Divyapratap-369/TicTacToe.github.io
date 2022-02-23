c31e681915f7d95209408811ea61ce0915eba9c2
Divya pratap Singh <64465398+Divyapratap-369@users.noreply.github.com>
2022-02-23 22:35:11 +0530

    Added Javascript file

---
+++ function.js
@@ -0,0 +1,4 @@
+// we need some event handlers to handle the events of clicking mouse eevent
+// status to save the state of the game
+// player change
+// and some functions to change player and restart game
