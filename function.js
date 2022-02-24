9b417dae57617e21b0eb04a1002cb6e7693fe3ac
Divya pratap Singh <64465398+Divyapratap-369@users.noreply.github.com>
2022-02-24 21:14:10 +0530

    Update function.js

--- function.js
+++ function.js
@@ -2,3 +2,16 @@
 // status to save the state of the game
 // player change
 // and some functions to change player and restart game
+const player_x = 'x'
+const Player_o = 'o'
+//winning combination if any combination made then declare winner
+const win_combinations = [
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[2, 4, 6]
+]
