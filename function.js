710c5b0bab5944eeba506598ffa9cfd49f198dfd
Divya pratap Singh <64465398+Divyapratap-369@users.noreply.github.com>
2022-03-20 22:43:06 +0530

    Update the javascript

--- function.js
+++ function.js
@@ -3,8 +3,7 @@
 // player change
 // and some functions to change player and restart game
 const player_x = 'x'
-const Player_o = 'o'
-//winning combination if any combination made then declare winner
+const player_o = 'circle'
 const win_combinations = [
 	[0, 1, 2],
 	[3, 4, 5],
@@ -15,3 +14,75 @@ const win_combinations = [
 	[0, 4, 8],
 	[2, 4, 6]
 ]
+const cellElements = document.querySelectorAll('[data-cell]')
+const boardElement = document.getElementById('board')
+const winningMessageElement = document.getElementById('winningMessage')
+const restartButton = document.getElementById('restartButton')
+const winningMessageTextElement = document.getElementById('winningMessageText')
+let isplayer_o_Turn = false
+startGame()
+
+restartButton.addEventListener('click', startGame)
+
+function startGame() {
+	isplayer_o_Turn = false
+	cellElements.forEach(cell => {
+		cell.classList.remove(player_x)
+		cell.classList.remove(player_o)
+		cell.removeEventListener('click', handleCellClick)
+		cell.addEventListener('click', handleCellClick, { once: true })
+	})
+	setBoardHoverClass()
+	winningMessageElement.classList.remove('show')
+}
+function handleCellClick(e) {
+	const cell = e.target
+	const currentClass = isplayer_o_Turn ? player_o : player_x
+	placeMark(cell, currentClass)
+	if (checkWin(currentClass)) {
+		endGame(false)
+	} else if (isDraw()) {
+		endGame(true)
+	} else {
+		swapTurns()
+		setBoardHoverClass()
+	}
+}
+function endGame(draw){
+	if(draw){
+		winningMessageTextElement.innerText = "It's a draw!"
+	}
+	else{
+		winningMessageTextElement.innerText = `player with ${isplayer_o_Turn?"O's":"x's"} wins`
+	}
+	winningMessageElement.classList.add(`show`)
+}
+function isDraw() {
+	return [...cellElements].every(cell => {
+		return cell.classList.contains(player_x) || cell.classList.contains(player_o)
+	})
+}
+function placeMark(cell, currentClass) {
+	cell.classList.add(currentClass)
+}
+
+function swapTurns() {
+	isplayer_o_Turn = !isplayer_o_Turn
+}
+function setBoardHoverClass() {
+	boardElement.classList.remove(player_x)
+	boardElement.classList.remove(player_o)
+	if (isplayer_o_Turn) {
+		boardElement.classList.add(player_o)
+	} else {
+		boardElement.classList.add(player_o)
+	}
+}
+function checkWin(currentClass) {
+	return WINNING_COMBINATIONS.some(combination => {
+		return combination.every(index => {
+			return cellElements[index].classList.contains(currentClass)
+		})
+	})
+}
+
